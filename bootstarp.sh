f86b0fda58f41ad549355237a894b0b4952643be
puneethprog9 <puneethreddyb@gmail.com>
2017-03-22 06:02:23 +0000

    bootstrapping a chef client

--- bootstarp.sh
+++ bootstarp.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 
-rm -f Instances.txt
 
 
-aws ec2 describe-instances --filter "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].[PublicIpAddress,BlockDeviceMappings[*].Ebs.AttachTime]' --output text  | sed 'N;s/\n/ /' >> Instances.txt
+aws ec2 describe-instances --filter "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].[PublicIpAddress,BlockDeviceMappings[*].Ebs.AttachTime]' --output text  | sed 'N;s/\n/ /' > Instances.txt
 
 
 IFS=$'\n'
@@ -19,6 +18,8 @@ for Instance in $(cat Instances.txt); do
 
   if [ $atime == $rtime ]; then
  echo knife bootstrap $ip -X ec2-user -i boot.pem -N node
+ else
+   echo $ip is not today system
 fi
 
 
